1d7a9df942510568fbeff5b04f86fbe44fe218cb
Gabriella_Akpo <ella9894@gmail.com>
2022-05-11 15:21:56 -0400

    initial inquirer commit

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template');
-const profileDataArgs = process.argv.slice(2);
-const [name, github] = profileDataArgs;
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
+// const pageHTML = generatePage(name, github);
+
+inquirer.prompt([{
+    type: 'input',
+    name: 'name',
+    message: 'What is your name?'
+}])
+    .then(answers => console.log(answers));
 
 
 
 
 
-
-
-fs.writeFile('./index.html',generatePage(name, github), err => {
-    if (err) throw err;
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+// fs.writeFile('./index.html',pageHTML, err => {
+//     if (err) throw err;
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
